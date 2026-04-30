80db5f28521e56ca8f22e55ef5416d97c6371a58
Thompson <tun78919@temple.edu>
2026-04-30 15:11:05 -0400

    Added a comment and note for developers.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PlanTime
 A hosted application that allows users to better plan their schedules and manage tasks.
+
+
+# Developer Notes
+Route example of when a user accesses the homepage: User accesses the site and then FastAPI runs the / route. This then returns templates/index.html as seen in the @app.get("/") function, meaning that the HTML loads. Inside index.html, the two lines which reference the style.css and app.js files are read by the browser. The browser then requests those static files. In return, FastAPI then serves them via the mount statement in main.py.
--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 # Creates a fastapi instance
 app = FastAPI()
-# Mounted to the /static path, refers to the "static directory", referred by FastAPI as static. If a there is a request to a file with /static, serve it from the static/ folder directly with FastAPI.
+# Mounts the static folder to the /static URL path, referred by FastAPI as static. If a there is a request to a file with /static, FastAPI serves it from the static/ folder directly via mount.
 app.mount("/static", StaticFiles(directory="static"), name="static")
 # Create an instance, templates, to later render and return a TemplateResponse. All html files are in templates directory.
 templates = Jinja2Templates(directory="templates")
 
-# root() runs whenever "/" path occurs. Route returns HTML. root() passes an instance of the Request object named request. Returns template instance.
+def render_page():
+    return
+# root() runs whenever FastAPI requests the "/" path. Route returns HTML. root() passes an instance of the Request object named request. Returns template instance.
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
